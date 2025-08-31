10f1fa2656b3b9d6f5eeb81fc330fd8cf067bb44
Musa Daniel <jtccontractsservicesng@yahoo.com>
2025-08-31 13:46:48 +0100

    First commit of the application. with no functionality added

---
+++ src/main/java/com/jtc/gateway/logistic/GatewayJtcLogisticApplication.java
@@ -0,0 +1,13 @@
+package com.jtc.gateway.logistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayJtcLogisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayJtcLogisticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtc/gateway/logistic/GatewayJtcLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtc.gateway.logistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayJtcLogisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
