b92ecf25ccfe45c99e20d88a62a565c2e4b5c58a
Musa Daniel <jtccontractsservicesng@yahoo.com>
2025-08-31 14:37:08 +0100

    Building the CI/CD pipeline. the initial workflows file created but not complete

--- src/main/java/com/jtc/gateway/logistic/GatewayJtcLogisticApplication.java
+++ src/main/java/com/jtc/gateway/logistic/GatewayJtcLogisticApplication.java
@@ -2,8 +2,11 @@ package com.jtc.gateway.logistic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class GatewayJtcLogisticApplication {
 
 	public static void main(String[] args) {
